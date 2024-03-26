c0c68c95ab4f7659102afb923240718493e617ad
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-03-26 19:08:41 +0800

    Craete React App & Install Bootstrap

---
+++ personal-website/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App()
+{
+    return (<div>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
+
+           
+    </div>)
+}
+
+export default App;
