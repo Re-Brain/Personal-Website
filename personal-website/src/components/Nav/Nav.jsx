cc48a915f7d0a4170c43b5a33521b96c9254fbb9
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-04-01 23:12:44 +0800

    Finish all responsive in different devices

--- personal-website/src/components/Nav/Nav.jsx
+++ personal-website/src/components/Nav/Nav.jsx
@@ -4,6 +4,7 @@ function Nav()
 {
     return (
         <nav id="nav-container">
+            <input type='checkbox' id='check' />
             <ul id="navbar" >
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
@@ -11,7 +12,9 @@ function Nav()
                 <li><a href="#work">Work</a></li>
                 <li><a href="#experience">Experience</a></li>
                 <li><a href="#contact">Contact</a></li>
+                <label for="check" class="close-menu"><i class="fas fa-times"></i></label>
             </ul>
+            <label for="check" class="open-menu"><i class="fas fa-bars"></i></label>
         </nav>
     )
 }
--- personal-website/src/sectionScroll.js
+++ personal-website/src/sectionScroll.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navigationHeight = document.querySelector('#nav-container').offsetHeight;
     
-    document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
+    document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 10 + "px");
 });
