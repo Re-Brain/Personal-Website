de1973d5c12ce7f1a103759cb8805b0dc9538b73
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-04-02 22:07:44 +0800

    Change the order of experience

--- personal-website/src/components/Experience/Experience.jsx
+++ personal-website/src/components/Experience/Experience.jsx
@@ -5,6 +5,11 @@ function Experience()
 {
     const items = [
         {
+            header : "Web-based Database Developer", 
+            content : ["Join the development team of Sukhothai Thammathirat Open University's web-based plant database.",
+                       "Assign to develop the university's Rajamangalaphisek Education Park plant database."]
+        },
+        {
             header : "Teacher Assistant Lab of Programming", 
             content : ["Provide hands-on support to students in the course",
                        "Explain and clarify programming concepts and assignments in C/C++",
@@ -13,12 +18,6 @@ function Experience()
         {
             header : "House Keeping", 
             content : ["Organize and maintained clean rooms to create a welcoming and comfortable environment for guests"]
-        }
-        ,
-        {
-            header : "Web-based Database Developer", 
-            content : ["Join the development team of Sukhothai Thammathirat Open University's web-based plant database.",
-                       "Assign to develop the university's Rajamangalaphisek Education Park plant database."]
         }
     ]
     return (
