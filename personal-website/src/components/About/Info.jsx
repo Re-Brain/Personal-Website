fac35049740a5aea73641c91db33b6204adcdd1f
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-04-09 09:41:10 +0800

    Change expert to expertist in about

--- personal-website/src/components/About/Info.jsx
+++ personal-website/src/components/About/Info.jsx
@@ -7,7 +7,7 @@ function Info()
             <p id="about-info-header">About Me</p>
             <p id="about-info-content">
                 As a driven and eager third-year student at National Dong Hwa University majoring in computer science and information engineering,
-                I'm actively looking for an internship that would expose me to real-world difficulties. My objective is to learn from experts. 
+                I'm actively looking for an internship that would expose me to real-world difficulties. My objective is to learn from expertist. 
                 Along with having hands-on expertise with both front-end and back-end web development technologies, 
                 I have a strong understanding of core programming concepts, data structures, and algorithms. 
                 In addition, I am proficient with Python frameworks and have some expertise with mobile programming. 
