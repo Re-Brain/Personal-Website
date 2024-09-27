19ee44837d7875f66bbe4c817e51080349595f53
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2024-09-27 17:26:20 +0800

    Change subheader of home

--- personal-website/src/components/Home/Profiletext.jsx
+++ personal-website/src/components/Home/Profiletext.jsx
@@ -5,7 +5,7 @@ function Profiletext()
     return (
         <div id="profile-text-container">
             <p id="profile-text-header">Hello, I'm Brain</p>
-            <p id="profile-text-subheader">Developer & AI</p>
+            <p id="profile-text-subheader">Software Engineer</p>
             <p className="profile-text-content">Bachelor Degree in Computer Science and Information Engineering</p>
             <p className="profile-text-content">National Dong Hwa University</p>
             <a href="#contact-container">
